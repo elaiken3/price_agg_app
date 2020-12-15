7f3652bb4f8b72666993b3fa64c04fbf44713695
Eugene Aiken <elaiken3@gmail.com>
2020-12-14 21:12:20 -0500

    12/14 Commit

--- price_agg_app.py
+++ price_agg_app.py
@@ -2,22 +2,34 @@
 from googleapiclient.discovery import build
 import pandas as pd
 import config
+import sys
+sys.executable
 
 api_key = config.api_key
 cse_id = config.cse_id
 
 #Google Query Function
-def google_query(query, api_key, cse_id, **kwargs):
+def google_query(item, api_key, cse_id, **kwargs):
+    df = pd.read_excel('./'+file)
+    df['Product_Name'] = df['Main Vendor'] + ' ' + df['Description']
+    item_list=[]
+    for item in df['Product_Name']:
+        item.append(df['Product_Name'])
+
+
     query_service = build("customsearch",
                           "v1",
                           developerKey=api_key
                           )
-    query_results = query_service.cse().list(q=query,    # Query
+    for item in item_list:
+        query_results = query_service.cse().list(q=item,    # Query
                                              cx=cse_id,  # CSE ID
                                              **kwargs
                                              ).execute()
 
-    return query_results['items']
+        print(query_results['items'])
+        return query_results['items']
+
 #End of Function
 
 
@@ -28,11 +40,11 @@ my_results_list_snippet=[]
 
 
 #External input for query
-query_name = str(input("Enter Search Name: "))
+file = str(input("Enter File Name:"))
 
 
 #Calling Google Query Function with external input for query
-my_results = google_query(query_name,
+my_results = google_query(file,
                           api_key,
                           cse_id,
                           num = 10
